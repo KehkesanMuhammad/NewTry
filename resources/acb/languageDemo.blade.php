b779fc6d84ae65684590b996a3cbd1c3af20f5db
Kehkesan Muhammad <kehkesan.muhammad@yahoo.com>
2021-05-21 10:52:18 +0500

    First one

---
+++ resources/acb/languageDemo.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    <h3>{{__('welcome')}}</h3><br><br> 
+    <h3>{{__('introduction')}}</h3>
+    </div>
+</div>
+@endsection
